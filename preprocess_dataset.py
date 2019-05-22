61e5268f396f5f2818aa0e94e585c332609c8ec1
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-05-22 14:09:51 -0700

    preprocess_dataset: slicing data tested on weizmann; [Action Required] -dev branch potential bug: rpath not updated when migrate file extension

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,65 +1,49 @@
+"""Preprocess datasets
 """
-"""
-import os
-import sys
-import copy
-import logging
-import operator
-import importlib
 
-from vdataset import constant
-from vdataset import metasets
-from vdataset import metadata
-from vdataset import preprocess
-from vdataset import mapreduce
+from torchstream.datasets import preprocess
+from torchstream.datasets.utils.mapreduce import Manager
+from torchstream.datasets.metadata.collect import collect_samples
+
+def slice_videos(name, samples, dst_root, ext="jpg",
+                 worker_num=16, **kwargs):
+    """
+    """
+    manager = Manager(name="Slicing Dataset [{}]".format(name),
+                      mapper=preprocess.vid2seq,
+                      retries=10,
+                      **kwargs
+                      )
+    manager.hire(worker_num=worker_num)
+    tasks = []
+    for src_sample in samples:
+        dst_sample = src_sample.root_migrated(dst_root)
+        dst_sample = dst_sample.extension_migrated(ext=ext)
+        tasks.append({"src_sample":src_sample, "dst_sample":dst_sample})
+    lens = manager.launch(tasks=tasks, enable_tqdm=True)
+    print(lens)
+    # result = np.histogram(lens, bins=1)
+    # print(result)
+
 
 def main(name):
-    mset = importlib.import_module("vdataset.metasets.{}".format(name))
-    collector = metadata.Collector(mset.path.AVI_DATA_PATH, mset,
-                                   ext="avi")
-    src_sample_set = collector.collect_samples()
-    dst_sample_set = src_sample_set.root_migrated(mset.path.JPG_DATA_PATH)
-    
-    src_sample_list = src_sample_set.get_samples()
-    src_sample_list.sort()
+    """
+    """
+    import importlib
+    name = "weizmann"
+    metaset = "torchstream.datasets.metadata.metasets.{}".format(name)
+    metaset = importlib.import_module(metaset)
 
-    dst_sample_list = dst_sample_set.get_samples()
-    dst_sample_list.sort()
+    kwargs = {
+        "root" : metaset.AVI_DATA_PATH,
+        "layout" : metaset.__layout__,
+        "lbls" : metaset.__LABELS__,
+        "mod" : "RGB",
+        "ext" : "avi",
+    }
+    samples = collect_samples(**kwargs)
 
-
-    print("Santity Check")
-    _pairs = list(zip(src_sample_list, dst_sample_list))
-    for _i in _pairs:
-        ## 
-        #  
-        #  
-        _i[1].to_images(ext="jpg")
-        
-        if _i[0].name != _i[1].name:
-            print("{} - {}".format(_i[0].name, _i[1].name))
-            exit(1)
-
-    print("Main Jobs")
-    tasks = []
-    for _i in _pairs:
-        tasks.append({"src_sample" : _i[0], "dst_sample" : _i[1]})
-
-    manager = mapreduce.Manager(name="slicing-{}".format(name),
-                                ## 
-                                # 
-                                mapper=preprocess.vid2seq,
-                                retries=10
-                                )
-    ## 
-    #  
-    manager.hire(worker_num=50)
-
-    result = manager.launch(tasks=tasks)
-    
-    for _i, _status in enumerate(result):
-        if not _status:
-            print("Task-[{}] Failed".format(_i))
-            print(tasks[_i])
+    slice_videos(name, samples, dst_root=metaset.JPG_DATA_PATH)
 
 if __name__ == "__main__":
     main("sth_sth_v2")
