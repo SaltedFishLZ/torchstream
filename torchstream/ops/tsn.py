66ad64e13e059087b7a2c5210bb819347beae166
SaltedFishLZ <google-dl-platform@googlegroups.com>
2019-05-23 01:15:08 +0000

    torchstream: transform reconstructed

--- torchstream/ops/tsn.py
+++ torchstream/ops/tsn.py
@@ -6,14 +6,14 @@ import torch
 __supported_consensuses__ = ["avg", "identity"]
 
 class SegmentConsensus(torch.autograd.Function):
-    '''
+    """
     Segment Consensus Operator in paper [] Section 
-    '''
+    """
     def __init__(self, consensus, dim=1):
-        '''
+        """
         - consensus : consensus type
         - dim : which dimension the aggregation happens
-        '''
+        """
         # santity check
         assert (consensus in __supported_consensuses__), \
             "Unsupported consensus type"
@@ -23,23 +23,23 @@ class SegmentConsensus(torch.autograd.Function):
         self.shape = None
 
     def forward(self, input_tensor):
-        '''
+        """
         - input : PyTorch tensor
-        '''
+        """
         self.shape = input_tensor.size()
-        if self.consensus == 'avg':
+        if self.consensus == "avg":
             output = input_tensor.mean(dim=self.dim, keepdim=True)
-        elif self.consensus == 'identity':
+        elif self.consensus == "identity":
             output = input_tensor
         else:
             output = None
         return output
 
     def backward(self, grad_output):
-        if self.consensus == 'avg':
+        if self.consensus == "avg":
             grad_in = grad_output.expand(self.shape) \
                 / float(self.shape[self.dim])
-        elif self.consensus == 'identity':
+        elif self.consensus == "identity":
             grad_in = grad_output
         else:
             grad_in = None
@@ -51,7 +51,7 @@ class ConsensusModule(torch.nn.Module):
 
     def __init__(self, consensus, dim=1):
         super(ConsensusModule, self).__init__()
-        self.consensus = consensus if consensus != 'rnn' else 'identity'
+        self.consensus = consensus if consensus != "rnn" else "identity"
         self.dim = dim
 
     def forward(self, input):
--- torchstream/transforms/transform/transform.py
+++ torchstream/transforms/transform/transform.py
@@ -26,26 +26,26 @@ __test__ = True
 # -------------------------------- #
 
 class VideoRandomCrop(object):
-    '''
+    """
     Random crop an certain area in the [H][W] dimension and
     keep all frames in a crop consistent in [H][W]
-    '''
+    """
     def __init__(self, size):
-        '''
+        """
         Initialization function
         - size : an integer S or a tuple (H, W)
-        '''
+        """
         if isinstance(size, numbers.Number):
             self.size = (int(size), int(size))
         else:
             self.size = size
 
     def __call__(self, varray):
-        '''
+        """
         Video level random crop
         - varray : video in a numpy ndarray, data layout is [T][H][W][C]
         - return : a cropped varray with the same data layout
-        '''
+        """
         h, w = varray.shape[1:3]
         th, tw = self.size
         # short cut
@@ -62,29 +62,29 @@ class VideoRandomCrop(object):
         return(result)
 
     def __repr__(self):
-        return self.__class__.__name__ + '(size={})'.format(self.size)
+        return self.__class__.__name__ + "(size={})".format(self.size)
 
 class VideoCenterCrop(object):
-    '''
+    """
     Crop the center area in the [H][W] dimension and
     keep all frames in a crop consistent in [H][W]    
-    '''
+    """
     def __init__(self, size):
-        '''
+        """
         Initialization function
         - size : an integer S or a tuple (H, W)
-        '''
+        """
         if isinstance(size, numbers.Number):
             self.size = (int(size), int(size))
         else:
             self.size = size
     
     def __call__(self, varray):
-        '''
+        """
         Video level random crop
         - varray : video in a numpy ndarray, data layout is [T][H][W][C]
         - return : a cropped varray with the same data layout
-        '''
+        """
         h, w = varray.shape[1:3]
         th, tw = self.size
         # short cut
@@ -101,7 +101,7 @@ class VideoCenterCrop(object):
         return(result)
 
     def __repr__(self):
-        return self.__class__.__name__ + '(size={})'.format(self.size)
+        return self.__class__.__name__ + "(size={})".format(self.size)
 
 
 
@@ -110,15 +110,15 @@ class VideoCenterCrop(object):
 # -------------------------------- #
 
 class VideoResize(object):
-    '''
-    Resize a video via OpenCV's resize API
+    """
+    Resize a video via OpenCV"s resize API
     NOTE: Currently, we only support spatial resize.
-    '''
+    """
     def __init__(self, size, interpolation=cv2.INTER_LINEAR):
-        '''
+        """
         - size 
         - interpolation
-        '''
+        """
         if isinstance(size, numbers.Number):
             self.size = (int(size), int(size))
         else:
@@ -126,13 +126,13 @@ class VideoResize(object):
         self.interpolation = interpolation
     
     def __call__(self, varray):
-        '''
+        """
         - varray : [T][H][W][C]
         - return : [T][H][w][C]
-        '''
+        """
         t, h, w, c = varray.shape
         result_shape = (t, self.size[0], self.size[1], c)
-        result = np.empty(result_shape, np.dtype('float32'))
+        result = np.empty(result_shape, np.dtype("float32"))
         for _i in range(t):
             farray = varray[_i, :, :, :]
             result[_i, :, :, :] = cv2.resize(farray, 
@@ -141,7 +141,7 @@ class VideoResize(object):
         return(result)
 
     def __repr__(self):
-        return self.__class__.__name__ + '(size={}, intrpl={})'.\
+        return self.__class__.__name__ + "(size={}, intrpl={})".\
             format(self.size, self.interpolation)
 
 
@@ -163,24 +163,24 @@ class VideoResize(object):
 # flow, which is "flow = - flow", rather than "flow = 
 # flip(flow)"
 class VideoFlip(object):
-    '''
+    """
     Video level deterministic flip
-    '''
+    """
     def __init__(self, dim="H"):
-        '''
+        """
         Initialization
         - dim : "H", "W" or "T", which dimension the flip
         takes place. "H" is height, "W" is width, "T" is time.
-        '''
+        """
         assert (dim in ["H", "W", "T"]), "Unsupported flip dimension"
         self.dim = copy.deepcopy(dim)
     
     def __call__(self, varray):
-        '''
+        """
         Flip a video, must execute
         - varray : [T][H][W][C]
         - return : flipped video
-        '''
+        """
         if ("T" == self.dim):
             return(np.flip(varray, 0))
         elif ("H" == self.dim):
@@ -191,23 +191,23 @@ class VideoFlip(object):
             assert True, "Error in transform"
 
     def __repr__(self):
-        return self.__class__.__name__ + '(dim={})'.format(self.dim)
+        return self.__class__.__name__ + "(dim={})".format(self.dim)
 
 
 class VideoRandomFlip(VideoFlip):
-    '''
+    """
     Video level random flip
-    '''
+    """
     def __init__(self, dim="H"):
         super(VideoRandomFlip, self).__init__(dim=dim)
     
     def __call__(self, varray):
         # TODO: how to notify the caller whether the video is flipped ?
-        '''
+        """
         Flip a video, execute with a probability of 0.5
         - varray : [T][H][W][C]
         - return : flipped video
-        '''
+        """
         v = np.random.random()
         # identical
         if (v < 0.5):
@@ -230,33 +230,33 @@ class VideoRandomFlip(VideoFlip):
 # -------------------------------- #
 
 class VideoNormalize(object):
-    '''
+    """
     Normalize all pixels of a video for each channel
-    '''
+    """
     def __init__(self, means, stds):
-        '''
+        """
         Initialization
         - means : pixel mean values for all pixels ([T][H][W]) in
         different channels
         - stds : pixel standard deviations for all pixels in different
         channels
-        '''
+        """
         self.means = copy.deepcopy(means)
         self.stds = copy.deepcopy(stds)
 
     def __call__(self, varray):
-        '''
+        """
         Normalize a video for each channel
         - varray : input video as a Numpy ndarray in [T][H][W][C] format
         - return : a normalized varray with the same format as input
-        '''
+        """
         (_t, _h, _w) = varray.shape[0:3]
         result = varray - np.tile(self.means, (_t, _h, _w, 1))
         result = varray / np.tile(self.stds, (_t, _h, _w, 1))
         return(result)
 
     def __repr__(self):
-        return self.__class__.__name__ + '(means={}, stds={})'.\
+        return self.__class__.__name__ + "(means={}, stds={})".\
             format(self.means, self.stds)
 
 
@@ -265,26 +265,26 @@ class VideoNormalize(object):
 # -------------------------------- #
 
 class ToTensor(object):
-    '''
+    """
     Convert a video sequence ndarray which is stored as [T][H][W][C] to 
     PyTorch float tensor [T][C][H][W].
     NOTE: Orginal video pixel values are np.uint8, in [0, 255], if you want
-    to scale the value to [0, 1], please specify the 'scale' argument in 
+    to scale the value to [0, 1], please specify the "scale" argument in 
     __init__ with True. If use normalization before, there is no need to scale.
-    '''
+    """
     def __init__(self, val_scale=False):
-        '''
+        """
         Initialization function
         - scale : whether the input blob will be scaled from [0,255] to [0,1]
-        '''
+        """
         self.val_scale = val_scale
 
     def __call__(self, varray):
-        '''
+        """
         Transform a varray to a PyTorch tensor, while make sure the data 
         layout is right.
         - varray : input video array as a Numpy ndarray, [T][H][W][C]
-        '''    
+        """    
         ret = torch.from_numpy(varray).permute(3, 2, 0, 1).contiguous()
         if (self.val_scale):
             ret = ret.div(255.0)
@@ -303,8 +303,8 @@ def test_transforms(test_configuration):
     vid_path = os.path.join(dir_path, "test.avi")
     # read video to varray
     varray = video2ndarray(vid_path,
-            color_in=test_configuration['video_color'],
-            color_out=test_configuration['varray_color'])
+            color_in=test_configuration["video_color"],
+            color_out=test_configuration["varray_color"])
     # # test video crop
     # crop = VideoRandomCrop(size=(128,171))
     # varray = crop(varray)
@@ -314,7 +314,7 @@ def test_transforms(test_configuration):
     #     winname = "{}".format(_i)
     #     farray_show(winname, varray[_i,:,:,:])
     #     cv2.moveWindow(winname, 40,30) 
-    #     (cv2.waitKey(0) & 0xFF == ord('q'))
+    #     (cv2.waitKey(0) & 0xFF == ord("q"))
     #     cv2.destroyAllWindows()
 
     # # test video flip
@@ -326,7 +326,7 @@ def test_transforms(test_configuration):
     #     winname = "{}".format(_i)
     #     farray_show(winname, varray[_i,:,:,:])
     #     cv2.moveWindow(winname, 40,30) 
-    #     (cv2.waitKey(0) & 0xFF == ord('q'))
+    #     (cv2.waitKey(0) & 0xFF == ord("q"))
     #     cv2.destroyAllWindows()
 
     # test video resize
@@ -338,7 +338,7 @@ def test_transforms(test_configuration):
         winname = "{}".format(_i)
         farray_show(winname, varray[_i,:,:,:])
         cv2.moveWindow(winname, 40,30) 
-        (cv2.waitKey(0) & 0xFF == ord('q'))
+        (cv2.waitKey(0) & 0xFF == ord("q"))
         cv2.destroyAllWindows()
 
 
@@ -348,10 +348,10 @@ if __name__ == "__main__":
     if (__test__):
 
         test_configuration = {
-            'video_color'   : "BGR",
-            'varray_color'  : "RGB",
-            'frames_color'  : "BGR",
-            'imgseq_color'  : "RGB"
+            "video_color"   : "BGR",
+            "varray_color"  : "RGB",
+            "frames_color"  : "BGR",
+            "imgseq_color"  : "RGB"
         }
 
         test_transforms(test_configuration)
