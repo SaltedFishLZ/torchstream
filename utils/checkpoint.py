7ef1fdb4098134bec2e6ffc3ff0cce406ba9d41a
Zheng Liang <google-dl-platform@googlegroups.com>
2019-06-15 01:39:03 +0000

    updated

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -7,4 +7,4 @@ def save_checkpoint(state, is_best, dir_path, pth_name="model"):
     file_path = os.path.join(dir_path, pth_name)
     torch.save(state, file_path + ".pth")
     if is_best:
-        shutil.copyfile(name, file_path + ".best.pth")
+        shutil.copyfile(file_path + ".pth", file_path + ".best.pth")
