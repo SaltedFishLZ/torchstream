3fbb4178bd6c8d3bcef5824def070db571f2c8d2
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-06-17 18:41:23 -0700

    interpolate updated

--- kfs/ops/interpolate.py
+++ kfs/ops/interpolate.py
@@ -10,16 +10,32 @@ class TemporalInterpolationFunction(torch.autograd.Function):
 
     """
     @staticmethod
-    def forward(ctx, input, index):
+    def forward(ctx, u, i):
         """
-        input [N][T][C][H][W]
+        u [N][Ti][C][H][W]
+        i [N][To], value belongs to [0, 1]
         """
-        T = input.size(dim)
-        left = torch.floor(index * T).long()
-        right = left + 1
-        last = (left >= (T - 1))
-        f_left = torch.index_select(input=input, )
-        output = last.float() * 
+        N, Ti, C, H, W = u.size()
+        N, To = i.size()
+        L = Ti - 1
+
+        i_l = torch.floor(i * L).long()
+        i_r = i_l + 1
+        
+        u_l = torch.empty(N, To, C, H, W)
+        u_r = torch.empty(N, To, C, H, W)
+        for n in range(N):
+            u_l[n, :, :, :, :] = u[n, i_l[n], :, :, :]
+            u_r[n, :, :, :, :] = u[n, i_r[n], :, :, :]
+
+        v = torch.empty(N, To, C, H, W)
+        for n in range(N):
+            for t in range(To):
+                v[n, t, :, :, :] = \
+                    (i_r[n, t] - i[n, t] * L) * u_l[n, t] + \
+                    (i[n, t] * L - i_l[n, t]) * u_r[n, t]
+        
+        return v
 
 
     @staticmethod
@@ -36,3 +52,13 @@ class TemporalInterpolationFunction(torch.autograd.Function):
         
         if ctx.needs_input_grad[1]:
             grad_output * (input(left) - input(right)) / (r - l)
+
+temporal_interpolation = TemporalInterpolationFunction.apply
+
+if __name__ == "__main__":
+    N, T, C, H, W = 1, 10, 3, 224, 224
+    u = torch.empty(N, T, C, H, W)
+    index = torch.Tensor([0.5])
+    v = temporal_interpolation(u, index)
+    output = v.sum()
+    
