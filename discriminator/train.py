01a0b7a120e28dacb27573330893d4f7dd38fd67
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-07-07 16:00:45 -0700

    [discriminator] configs & train script updated

--- discriminator/train.py
+++ discriminator/train.py
@@ -1,7 +1,5 @@
 """
 """
-import os
-import gc
 import time
 import json
 import argparse
@@ -12,7 +10,6 @@ import torchstream
 import cfgs
 import utils
 
-import kfs
 
 val_log_str = "Validation:[{:4d}/{:4d}],  " + \
               "BatchTime:{batch_time.val:6.2f}({batch_time.avg:6.2f}),  " + \
@@ -21,9 +18,10 @@ val_log_str = "Validation:[{:4d}/{:4d}],  " + \
               "Prec@1:{top1_meter.val:7.3f}({top1_meter.avg:7.3f}),  " + \
               "Prec@5:{top5_meter.val:7.3f}({top5_meter.avg:7.3f})"
 
+
 def validate(device, loader, model, criterion,
              log_str=val_log_str, log_interval=20, **kwargs):
-    
+
     batch_time = utils.Meter()
     data_time = utils.Meter()
     loss_meter = utils.Meter()
@@ -67,12 +65,16 @@ def validate(device, loader, model, criterion,
                                      top1_meter=top1_meter,
                                      top5_meter=top5_meter))
 
-
-    print("Results:\n" + \
-          "Prec@1 {top1_meter.avg:5.3f} Prec@5 {top5_meter.avg:5.3f} Loss {loss_meter.avg:5.3f}"
-          .format(top1_meter=top1_meter, top5_meter=top5_meter, loss_meter=loss_meter))
+    print("Results:\n"
+          "Prec@1 {top1_meter.avg:5.3f} "
+          "Prec@5 {top5_meter.avg:5.3f} "
+          "Loss {loss_meter.avg:5.3f}"
+          .format(top1_meter=top1_meter,
+                  top5_meter=top5_meter,
+                  loss_meter=loss_meter))
 
     return top1_meter.avg
+
 
 train_log_str = "Epoch:[{:3d}][{:4d}/{:4d}],  lr:{lr:5.5f},  " + \
                 "BatchTime:{batch_time.val:6.2f}({batch_time.avg:6.2f}),  " + \
@@ -83,7 +85,7 @@ train_log_str = "Epoch:[{:3d}][{:4d}/{:4d}],  lr:{lr:5.5f},  " + \
 
 
 def train(device, loader, model, criterion, optimizer, epoch,
-                log_str=train_log_str, log_interval=20, **kwargs):
+          log_str=train_log_str, log_interval=20, **kwargs):
 
     batch_time = utils.Meter()
     data_time = utils.Meter()
@@ -97,7 +99,7 @@ def train(device, loader, model, criterion, optimizer, epoch,
 
     end = time.time()
 
-    for i, (input, target) in enumerate(loader):        
+    for i, (input, target) in enumerate(loader):
         input = input.to(device)
         target = target.to(device)
 
@@ -120,7 +122,7 @@ def train(device, loader, model, criterion, optimizer, epoch,
 
         # backward
         optimizer.zero_grad()
-        loss.backward() 
+        loss.backward()
         optimizer.step()
 
         # measure elapsed time on GPU
@@ -137,9 +139,6 @@ def train(device, loader, model, criterion, optimizer, epoch,
                                  lr=optimizer.param_groups[-1]['lr']))
 
 
-
-
-
 def main(args):
 
     # args, configs -> configs
@@ -154,7 +153,6 @@ def main(args):
 
     device = torch.device("cuda:0")
     best_prec1 = 0
-
 
     # -------------------------------------------------------- #
     #          Construct Datasets & Dataloaders                #
@@ -173,7 +171,6 @@ def main(args):
     configs["train_loader"]["dataset"] = train_dataset
     train_loader = cfgs.config2dataloader(configs["train_loader"])
 
-
     val_transforms = []
     for _t in configs["val_transforms"]:
         val_transforms.append(cfgs.config2transform(_t))
@@ -187,26 +184,27 @@ def main(args):
     configs["val_loader"]["dataset"] = val_dataset
     val_loader = cfgs.config2dataloader(configs["val_loader"])
 
-
-
     # -------------------------------------------------------- #
     #             Construct Network & Optimizer                #
     # -------------------------------------------------------- #
 
-    model = kfs.Wrapper(**configs["model"]["argv"])
+    model = cfgs.config2model(configs["model"])
     model.to(device)
 
     model = torch.nn.DataParallel(model, device_ids=configs["gpus"])
 
-    configs["optimizer"]["argv"]["params"] = model.parameters()
+    if configs["optimizer"]["argv"]["params"] == "model_specified":
+        print("Use Model Specified Training Policies")
+        configs["optimizer"]["argv"]["params"] = \
+            model.module.get_optim_policies()
+    else:
+        configs["optimizer"]["argv"]["params"] = model.parameters()
     optimizer = cfgs.config2optimizer(configs["optimizer"])
 
     lr_scheduler = cfgs.config2lrscheduler(optimizer, configs["lr_scheduler"])
 
     criterion = cfgs.config2criterion(configs["criterion"])
     criterion.to(device)
-
-
 
     # -------------------------------------------------------- #
     #            Resume / Finetune from Checkpoint             #
@@ -226,34 +224,22 @@ def main(args):
             model.load_state_dict(model_state_dict)
             optimizer.load_state_dict(optimizer_state_dict)
             lr_scheduler.load_state_dict(lr_scheduler_state_dict)
-            print("Resume from epoch [{}], best prec1 [{}]".format(start_epoch - 1, best_prec1))
+            print("Resume from epoch [{}], best prec1 [{}]".
+                  format(start_epoch - 1, best_prec1))
         else:
             print("failed to load checkpoint")
 
-    if "classifier" in configs["train"]:
-        classifier_config = configs["train"]["classifier"]
-        if "pretrained" in classifier_config:
-            checkpoint = utils.load_checkpoint(**(classifier_config["pretrained"]))
-            # load state_dict
-            classifier_state_dict = checkpoint["model_state_dict"]
-            old_keys = list(classifier_state_dict.keys())
-            for old_key in old_keys:
-                new_key = old_key.replace("module.", "")
-                val = classifier_state_dict[old_key]
-                del classifier_state_dict[old_key]
-                classifier_state_dict[new_key] = val
-            model.module.classifier.load_state_dict(classifier_state_dict)
-        if "freeze" in classifier_config:
-            if classifier_config["freeze"]:
-                print("Freezing Classifier...")
-                model.module.freeze_classifier()
+    elif "finetune" in configs["train"]:
+        finetune_config = configs["train"]["finetune"]
+        checkpoint = utils.load_checkpoint(**finetune_config)
 
-    # -------------------------------------------------------- #
-    #                       Echo Config                        #
-    # -------------------------------------------------------- #
-    print(optimizer)
-    print(lr_scheduler)
-
+        model_state_dict = checkpoint["model_state_dict"]
+        for key in model_state_dict:
+            if "fc" in key:
+                # use FC from new network
+                print("Replacing ", key)
+                model_state_dict[key] = model.state_dict()[key]
+        model.load_state_dict(model_state_dict)
 
     # -------------------------------------------------------- #
     #                       Main Loop                          #
@@ -270,16 +256,16 @@ def main(args):
               criterion=criterion, optimizer=optimizer,
               epoch=epoch)
 
-        lr_scheduler.step()
-
         # evaluate on validation set
         prec1 = validate(device=device, loader=val_loader, model=model,
                          criterion=criterion, epoch=epoch)
 
-        # remember best prec@1 
+        # remember best prec@1
         is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1) 
+        best_prec1 = max(prec1, best_prec1)
         print("Best Prec@1: %.3f\n" % (best_prec1))
+
+        lr_scheduler.step()
 
         # save checkpoint
         if backup_config is not None:
@@ -298,11 +284,9 @@ def main(args):
                                   pth_name=pth_name)
 
 
-
-
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="PyTorch Video Recognition Template")
+    parser = argparse.ArgumentParser(description="Template Training Script")
     # configuration file
     parser.add_argument("config", type=str,
                         help="path to configuration file")
