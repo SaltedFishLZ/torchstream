9c67b89fb75767e3631f0e961af7957c17e52747
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-08-24 20:35:01 -0700

    ddp: add checkpoint loading checking

--- experiments/template/train.py
+++ experiments/template/train.py
@@ -261,13 +261,19 @@ def worker(pid, ngpus_per_node, args):
         # NOTE: the 1st place to load checkpoint
         resume_config = configs["train"]["resume"]
         checkpoint = utils.load_checkpoint(**resume_config)
-        # if (pid == 8):
-        #     print(checkpoint)
-        #     exit(0)
         if checkpoint is None:
             print("Load Checkpoint Failed")
         if checkpoint is not None:
-            model.load_state_dict(checkpoint["model_state_dict"])
+            # check checkpoint device mapping
+            model_state_dict = checkpoint["model_state_dict"]
+            for _k in model_state_dict:
+                _v = model_state_dict[_k]
+                expected_device = "cuda:{}".format(args.gid)
+                if expected_device != _v.device:
+                    raise ValueError("Device Mismatch [{}]: {} -> {}".format(
+                        _k, expected_device, _v.device
+                    ))
+            model.load_state_dict(model_state_dict)
     # ignore finetune if there is a checkpoint
     if (checkpoint is None) and ("finetune" in configs["train"]):
         finetune_config = configs["train"]["finetune"]
