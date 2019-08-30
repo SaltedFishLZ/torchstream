d71660ee71cc405334745e876af2e3fd673e1864
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-08-30 02:30:07 -0700

    torchstream.datasets.hmdb51: new cleaned dataset, basic test passed

--- tests/torchstream/datasets/hmdb51/test_hmdb51.py
+++ tests/torchstream/datasets/hmdb51/test_hmdb51.py
@@ -1,9 +1,15 @@
 from torchstream.datasets.hmdb51 import HMDB51
 
+
 def test_hmdb51():
-    dataset = HMDB51(train=True)
+    # dataset_len = 6766
+    dataset_path = "~/Datasets/HMDB51/HMDB51-avi"
+    dataset = HMDB51(root=dataset_path, train=True)
     print(dataset.__len__())
+
+    for vid, cid in dataset:
+        print(cid)
 
 
 if __name__ == "__main__":
-    test()
+    test_hmdb51()
