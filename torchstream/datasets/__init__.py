aef11313efa05aa42c1df592d0daf0707d741118
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-09-04 01:05:10 -0700

    template config for sthsthv1 updated; regression testing scripts added for UCF101, HMDB51 & SthSthV1

--- torchstream/datasets/__init__.py
+++ torchstream/datasets/__init__.py
@@ -8,7 +8,7 @@ __all__ = ["HMDB51"]
 from .hmdb51 import HMDB51
 from .ucf101 import UCF101
 # from .jesterv1 import JesterV1
-# from .sthsthv1 import SomethingSomethingV1
+from .sthsthv1 import SomethingSomethingV1
 # from .sthsthv2 import SomethingSomethingV2
 # from .tinysthsthv1 import TinySomethingSomethingV1
 # from .tinysthsthv2 import TinySomethingSomethingV2
