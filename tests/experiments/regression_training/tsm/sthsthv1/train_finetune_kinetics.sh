a7d26cb85339903814e90617404651aa0723d1c1
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-09-04 03:12:09 -0700

    template training configs: sthsthv1 dataset jpg->avi

--- tests/experiments/regression_training/tsm/sthsthv1/train_finetune_kinetics.sh
+++ tests/experiments/regression_training/tsm/sthsthv1/train_finetune_kinetics.sh
@@ -2,7 +2,7 @@ source ../../venv_video/bin/activate
 
 # data prallel
 sudo mkdir -p logs/tsm/sthsthv1/resnet50/8x224x224/train/finetune/
-python train.py \
+python -u train.py \
     configs/tsm/sthsthv1/resnet50/8x224x224/train/finetune/kinetics0_lr5e-3_p9e-1_step20_b64_j64_e50.json \
     > >(sudo tee logs/tsm/sthsthv1/resnet50/8x224x224/train/finetune/kinetics0_lr5e-3_p9e-1_step20_b64_j64_e50.log)
 
