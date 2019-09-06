f0de63ce1e24a15bb2b68643674200bb311a6e95
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-09-06 11:50:43 -0700

    checkpoint download script updated (enable partial download)

--- experiments/template/checkpoints/download.bash
+++ experiments/template/checkpoints/download.bash
@@ -1,12 +1,27 @@
 #!/bin/bash
-echo "Downloading checkpoints"
-
+CUR_BASH_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 SERVER_PREFIX="zhen@a18.millennium.berkeley.edu:video-acc"
 
-
+if [ "$#" -eq 0 ];
+then
+    echo "download all checkpoints..."
     # if the source server delete something, local side will delete too
     rsync -avz --exclude "*.bash" \
-        "${SERVER_PREFIX}/pretrained_models/"" \
+        "${SERVER_PREFIX}/pretrained_models/" \
         ./ \
         --delete
+else
+    for path in "$@"
+    do
+        # make dir
+        mkdir -p "${CUR_BASH_DIR_PATH}/${path}"
+        # use rsync to download, the trailing `/` is necessary to avoid
+        # duplicating the folder
+        echo "download ${path}..."
+        rsync -avz --exclude "*.bash" \
+            "${SERVER_PREFIX}/pretrained_models/${path}/" \
+            ${path} \
+            --delete
+    done
+fi
