4668dae37b82c85985eb05b3f6e9977aa977fda8
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-09-07 12:11:06 -0700

    kinetics400 pretrained model testing passed

--- torchstream/datasets/__init__.py
+++ torchstream/datasets/__init__.py
@@ -1,12 +1,13 @@
 """Video Dataset Module
 """
-__authors__ = ["Zheng Liang @ Peking University"]
+__authors__ = ["Zheng Liang @ Peking University", "Bernie Wang @ UC Berkeley"]
 __author__ = __authors__
 __version__ = "0.1"
-__all__ = ["HMDB51"]
+__all__ = ["HMDB51", "UCF101", "JesterV1", "SomethingSomethingV1", "Kinetics400"]
 
 from .hmdb51 import HMDB51
 from .ucf101 import UCF101
+from .kinetics400 import Kinetics400
 from .jesterv1 import JesterV1
 from .sthsthv1 import SomethingSomethingV1
 # from .sthsthv2 import SomethingSomethingV2
