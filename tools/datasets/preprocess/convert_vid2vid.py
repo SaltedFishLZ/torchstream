68bf9a4b76e228a724134bbd4eed6ea1f89ac107
Zhen Dong <zhen@a21.Millennium.Berkeley.EDU>
2019-09-11 12:02:13 -0700

    preprocess tools: convert_vid2vid updated

--- tools/datasets/preprocess/convert_vid2vid.py
+++ tools/datasets/preprocess/convert_vid2vid.py
@@ -10,12 +10,12 @@ from torchstream.utils.mapreduce import Manager
 
 parser = argparse.ArgumentParser(description="Convert Dataset (vid2vid)")
 parser.add_argument("datapoints", type=str, help="path to source datapoints")
-parser.add_argument("dst", type=str, help="destination dataset root")
-parser.add_argument("--ext", type=str, help="destination dataset video ext")
+parser.add_argument("dst_root", type=str, help="destination dataset root")
+parser.add_argument("--dst_ext", type=str, help="destination dataset video ext")
 parser.add_argument("--workers", type=int, default=32, help="worker number")
 
 
-def dataset_vid2vid(name, datapoints, dst_root, dst_ext="avi",
+def dataset_vid2vid(name, src_datapoints, dst_root, dst_ext="avi",
                     workers=16, **kwargs):
     """transform video fils
     """
@@ -23,25 +23,28 @@ def dataset_vid2vid(name, datapoints, dst_root, dst_ext="avi",
                       mapper=vid2vid,
                       retries=10,
                       **kwargs)
-    manager.hire(workers=workers)
+    manager.hire(worker_num=workers)
     tasks = []
 
-    for src in datapoints:
+    dst_datapoints = []
+    for src in src_datapoints:
         assert isinstance(src, DataPoint), TypeError
         dst = copy.deepcopy(src)
         dst.root = dst_root
         dst.ext = dst_ext
         dst._path = dst.path
-        tasks.append({"src": src, "dste": dst})
+        dst_datapoints.append(dst)
+        tasks.append({"src": src, "dst": dst})
 
-    successes = manager.launch(tasks=tasks, enable_tqdm=True)
+    successes = manager.launch(tasks=tasks, progress=True)
 
     # remove failed videos from datapoints
     clean_datapoints = []
     for idx, success in enumerate(successes):
-        clean_datapoints.append(datapoints[idx])
+        clean_datapoints.append(dst_datapoints[idx])
+    dst_datapoints = clean_datapoints
 
-    return successes, clean_datapoints
+    return successes, dst_datapoints
 
 
 if __name__ == "__main__":
@@ -54,7 +57,7 @@ if __name__ == "__main__":
 
     successes, dst_datapoints = dataset_vid2vid(
         name=src_datapoints[0].root,
-        datapoints = src_datapoints,
+        src_datapoints=src_datapoints,
         dst_root=args.dst_root,
         dst_ext=args.dst_ext,
         workers=args.workers
@@ -62,9 +65,10 @@ if __name__ == "__main__":
 
     print("Total Videos: [{}]".format(len(src_datapoints)))
     failures = 0
-    for i, dp in enumerate(src_datapoints):
-        print("Failure: [{}]".format(i))
-        print(dp)
-        failures += 1
+    for idx, dp in enumerate(dst_datapoints):
+        if not successes[idx]:
+            print("Failure: [{}]".format(idx))
+            print(dp)
+            failures += 1
     print("Total Failures: [{}]".format(failures))
 
