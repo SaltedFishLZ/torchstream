26621e0ffa820fc15c57cd11a2711f0c8b03dd0e
SaltedFishLZ <liangzheng@pku.edu.cn>
2019-10-10 10:51:46 -0700

    test: dataset/hmdb51 test updated

--- tests/torchstream/datasets/hmdb51/test_hmdb51.py
+++ tests/torchstream/datasets/hmdb51/test_hmdb51.py
@@ -6,6 +6,7 @@ from torchstream.datasets.hmdb51 import HMDB51
 
 
 def test_hmdb51():
+    # split 1, training set
     # dataset_len = 6766
     dataset_path = "~/Datasets/HMDB51/HMDB51-avi"
     dataset = HMDB51(root=dataset_path,
@@ -13,11 +14,11 @@ def test_hmdb51():
                                         Resize(256),
                                         CenterCrop(224)]),
                      train=True)
-    print(dataset.__len__())
+    # assert dataset.__len__() == 6766, ValueError
 
     dataloader = torch.utils.data.DataLoader(dataset=dataset,
-                                             batch_size=32,
-                                             num_workers=32,
+                                             batch_size=16,
+                                             num_workers=4,
                                              pin_memory=True)
 
     num_samples_per_class = collections.OrderedDict()
@@ -28,6 +29,19 @@ def test_hmdb51():
             else:
                 num_samples_per_class[_cid] = 1
     print(num_samples_per_class)
+
+    return
+
+    # split 1, validation set
+    # dataset_len = 3750
+    dataset_path = "~/Datasets/HMDB51/HMDB51-avi"
+    dataset = HMDB51(root=dataset_path,
+                     transform=Compose([CenterSegment(32),
+                                        Resize(256),
+                                        CenterCrop(224)]),
+                     train=False)
+    assert dataset.__len__() == 3750, ValueError
+
 
 
 if __name__ == "__main__":
